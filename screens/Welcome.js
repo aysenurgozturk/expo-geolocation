1c8a7658c8dcc4379d4c26ed6643da80b1ce09c7
ali haydar <alihaydar@ali-MacBook-Air.local>
2021-07-16 14:16:32 +0300

    mesela yani

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,50 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Image, Alert } from 'react-native';
-import * as Location from 'expo-location';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Image, Alert, Dimensions } from "react-native";
+import * as Location from "expo-location";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 const Welcome = ({ navigation }) => {
+  const width = Dimensions.get("screen").width;
+  const height = Dimensions.get("screen").height;
+  const aspect = width / height;
+  const [location, setlocation] = React.useState({ latitude: 0, longitude: 0 });
+
   const [locationServiceEnabled, setLocationServiceEnabled] = useState(false);
   const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
-    'Wait, we are fetching you location...'
+    "Wait, we are fetching you location..."
   );
-
+  
   useEffect(() => {
     CheckIfLocationEnabled();
     GetCurrentLocation();
   }, []);
-  
+
   // create the handler method
-  
+
   const GetCurrentLocation = async () => {
-    let { status } = await Location.requestPermissionsAsync();
-  
-    if (status !== 'granted') {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== "granted") {
       Alert.alert(
-        'Permission not granted',
-        'Allow the app to use location service.',
-        [{ text: 'OK' }],
+        "Permission not granted",
+        "Allow the app to use location service.",
+        [{ text: "OK" }],
         { cancelable: false }
       );
     }
-  
+
     let { coords } = await Location.getCurrentPositionAsync();
-  
+    console.log(coords);
     if (coords) {
       const { latitude, longitude } = coords;
+      location.latitude = latitude;
+      location.longitude = longitude;
       let response = await Location.reverseGeocodeAsync({
         latitude,
-        longitude
+        longitude,
       });
-  
+
       for (let item of response) {
+        console.log(item);
         let address = `${item.name}, ${item.street}, ${item.postalCode}, ${item.city}`;
-  
+
         setDisplayCurrentAddress(address);
-        if (address.length > 0) {
-          setTimeout(() => {
-            navigation.navigate('Home', { item: address });
-          }, 2000);
-        }
       }
     }
   };
@@ -53,9 +57,9 @@ const Welcome = ({ navigation }) => {
 
     if (!enabled) {
       Alert.alert(
-        'Location Service not enabled',
-        'Please enable your location services to continue',
-        [{ text: 'OK' }],
+        "Location Service not enabled",
+        "Please enable your location services to continue",
+        [{ text: "OK" }],
         { cancelable: false }
       );
     } else {
@@ -64,9 +68,19 @@ const Welcome = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.container}>
+    <View style={{flex:1,justifyContent:"flex-start"}}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        region={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0922 * aspect,
+        }}
+        showsUserLocation={true}
+         style={{width:"100%",height:350}}
+      ></MapView>
       <View style={styles.contentContainer}>
-     
         <Text style={styles.title}>What's your address?</Text>
       </View>
       <Text style={styles.text}>{displayCurrentAddress}</Text>
@@ -77,30 +91,30 @@ const Welcome = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#070707',
-    alignItems: 'center',
-    paddingTop: 130
+    backgroundColor: "#070707",
+    alignItems: "center",
+    paddingTop: 130,
   },
   contentContainer: {
-    alignItems: 'center',
-    marginBottom: 20
+    alignItems: "center",
+    marginBottom: 20,
   },
   image: {
     width: 150,
     height: 150,
-    resizeMode: 'contain',
-    marginBottom: 20
+    resizeMode: "contain",
+    marginBottom: 20,
   },
   title: {
     fontSize: 22,
-    fontWeight: '700',
-    color: '#FD0139'
+    fontWeight: "700",
+    color: "#FD0139",
   },
   text: {
     fontSize: 20,
-    fontWeight: '400',
-    color: '#fff'
-  }
+    fontWeight: "400",
+    color: "#fff",
+  },
 });
 
 export default Welcome;
